c07598eb2bc418f08436179f5609e50312e525cc
Elena Kanysheva <elenakanicheva@gmail.com>
2024-07-31 23:17:57 +0500

    Homework20_1

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,40 @@
 from django.db import models
 
-# Create your models here.
+
+NULLABLE = {'blank': True, 'null': True}
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=150, verbose_name="Наименование")
+    description = models.TextField(verbose_name="Описание", **NULLABLE)
+
+    class Meta:
+        verbose_name = "Категория"
+        verbose_name_plural = "Категории"
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=150, verbose_name="Наименование")
+    description = models.TextField(verbose_name="Описание", **NULLABLE)
+    image = models.ImageField(upload_to="products/", verbose_name="Изображение", **NULLABLE)
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.SET_NULL,
+        **NULLABLE,
+        verbose_name="Категория",
+        related_name='products'
+    )
+    price = models.IntegerField(verbose_name="Цена", **NULLABLE)
+    created_at = models.DateField(verbose_name="Дата создания", auto_now_add=True)
+    updated_at = models.DateField(verbose_name="Дата последнего изменения", auto_now=True)
+
+    class Meta:
+        verbose_name = "Продукт"
+        verbose_name_plural = "Продукты"
+        ordering = ["category", "name"]
+
+    def __str__(self):
+        return self.name
